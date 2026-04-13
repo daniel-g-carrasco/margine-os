6e534801761f18183761381ffdd5ee82e5dec962
Daniel Grasso <daniel.g@mailbox.org>
2026-04-13 11:04:47 +0200

    fix: enforce transparent lazyvim background

--- files/home/.config/nvim/init.lua
+++ files/home/.config/nvim/init.lua
@@ -1,2 +1,61 @@
+local transparent_groups = {
+  "Normal",
+  "NormalNC",
+  "NormalFloat",
+  "FloatBorder",
+  "FloatTitle",
+  "FloatFooter",
+  "SignColumn",
+  "LineNr",
+  "EndOfBuffer",
+  "FoldColumn",
+  "StatusLine",
+  "StatusLineNC",
+  "TabLineFill",
+  "WinSeparator",
+  "VertSplit",
+  "Pmenu",
+  "PmenuSbar",
+  "PmenuThumb",
+  "NormalSB",
+  "NeoTreeNormal",
+  "NeoTreeNormalNC",
+  "NeoTreeWinSeparator",
+  "SnacksNormal",
+  "SnacksPicker",
+  "LazyNormal",
+  "MasonNormal",
+  "TelescopeNormal",
+  "TelescopeBorder",
+  "TelescopePromptNormal",
+  "TelescopePromptBorder",
+  "TelescopeResultsNormal",
+  "TelescopeResultsBorder",
+  "TelescopePreviewNormal",
+  "TelescopePreviewBorder",
+  "WhichKeyNormal",
+}
+
+local function apply_transparent_background()
+  for _, group in ipairs(transparent_groups) do
+    vim.api.nvim_set_hl(0, group, { bg = "NONE" })
+  end
+
+  vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "#babbf1", bg = "NONE", bold = true })
+end
+
+local transparent_augroup = vim.api.nvim_create_augroup("margine_transparent_nvim", { clear = true })
+
+vim.api.nvim_create_autocmd("ColorScheme", {
+  group = transparent_augroup,
+  pattern = "*",
+  callback = apply_transparent_background,
+})
+
+vim.api.nvim_create_autocmd("VimEnter", {
+  group = transparent_augroup,
+  callback = apply_transparent_background,
+})
+
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
--- files/home/.config/nvim/lua/plugins/colorscheme.lua
+++ files/home/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,50 +1,72 @@
+local transparent_groups = {
+  "Normal",
+  "NormalNC",
+  "NormalFloat",
+  "FloatBorder",
+  "FloatTitle",
+  "FloatFooter",
+  "SignColumn",
+  "LineNr",
+  "EndOfBuffer",
+  "FoldColumn",
+  "StatusLine",
+  "StatusLineNC",
+  "TabLineFill",
+  "WinSeparator",
+  "VertSplit",
+  "Pmenu",
+  "PmenuSbar",
+  "PmenuThumb",
+  "NormalSB",
+  "NeoTreeNormal",
+  "NeoTreeNormalNC",
+  "NeoTreeWinSeparator",
+  "SnacksNormal",
+  "SnacksPicker",
+  "LazyNormal",
+  "MasonNormal",
+  "TelescopeNormal",
+  "TelescopeBorder",
+  "TelescopePromptNormal",
+  "TelescopePromptBorder",
+  "TelescopeResultsNormal",
+  "TelescopeResultsBorder",
+  "TelescopePreviewNormal",
+  "TelescopePreviewBorder",
+  "WhichKeyNormal",
+}
+
+local function apply_transparency()
+  for _, group in ipairs(transparent_groups) do
+    vim.api.nvim_set_hl(0, group, { bg = "NONE" })
+  end
+
+  vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "#babbf1", bg = "NONE", bold = true })
+end
+
 return {
   {
     "catppuccin/nvim",
     name = "catppuccin",
+    priority = 1000,
+    init = function()
+      vim.api.nvim_create_autocmd("ColorScheme", {
+        pattern = "*",
+        callback = function()
+          vim.schedule(apply_transparency)
+        end,
+      })
+    end,
     opts = {
       flavour = "frappe",
       transparent_background = true,
+      float = {
+        transparent = true,
+        solid = false,
+      },
       custom_highlights = function(colors)
-        local transparent = { bg = "NONE" }
-
         return {
-          Normal = transparent,
-          NormalNC = transparent,
-          NormalFloat = transparent,
-          FloatBorder = transparent,
-          FloatTitle = transparent,
-          FloatFooter = transparent,
-          SignColumn = transparent,
-          LineNr = transparent,
           CursorLineNr = { fg = colors.lavender, bg = "NONE", bold = true },
-          EndOfBuffer = transparent,
-          FoldColumn = transparent,
-          StatusLine = transparent,
-          StatusLineNC = transparent,
-          TabLineFill = transparent,
-          WinSeparator = transparent,
-          VertSplit = transparent,
-          Pmenu = transparent,
-          PmenuSbar = transparent,
-          PmenuThumb = transparent,
-          NormalSB = transparent,
-          NeoTreeNormal = transparent,
-          NeoTreeNormalNC = transparent,
-          NeoTreeWinSeparator = transparent,
-          SnacksNormal = transparent,
-          SnacksPicker = transparent,
-          LazyNormal = transparent,
-          MasonNormal = transparent,
-          TelescopeNormal = transparent,
-          TelescopeBorder = transparent,
-          TelescopePromptNormal = transparent,
-          TelescopePromptBorder = transparent,
-          TelescopeResultsNormal = transparent,
-          TelescopeResultsBorder = transparent,
-          TelescopePreviewNormal = transparent,
-          TelescopePreviewBorder = transparent,
-          WhichKeyNormal = transparent,
         }
       end,
     },
